ad442d132081aa6c41e88738ac78c7e3e7cc2641
wagnerbauer <wagnerbauer@users.noreply.github.com>
2013-10-06 11:18:10 -0300

    TravessiaDesertoTest.java

--- TravessiaDesertoTest.java
+++ TravessiaDesertoTest.java
@@ -8,10 +8,13 @@ public class TravessiaDesertoTest {
 
 
 	private TravessiaDeserto travessia = null;
-	private int pos;
-	private int MAP_SIZE;
+	public int pos;
+	public int MAP_SIZE;
 	private int fuel;
 	private int map;
+	public static final int MAX_FUEL = 6;
+	public static final int MIN_FUEL = 0;
+
 	
 	@Before
 	public void setUp() {
@@ -35,6 +38,7 @@ public class TravessiaDesertoTest {
 		
 		assertEquals(combustivel, travessia.getFuel() - 1);
 		assertEquals(posicao - 1, travessia.getPos());
+		System.out.println("voltarTest ok");
 	}
 
 	@Test
@@ -48,6 +52,7 @@ public class TravessiaDesertoTest {
 
 		assertEquals(combustivel - 1, travessia.getFuel());
 		assertEquals(posicao + 1, travessia.getPos());
+		System.out.println("avancarTest ok");
 	}
 	@Test
 	public void initGameTest() {
@@ -55,32 +60,67 @@ public class TravessiaDesertoTest {
 		assertEquals(0, travessia.getPos());
 		assertEquals(travessia.MAX_FUEL, travessia.getFuel());
 		assertEquals(travessia.MAP_SIZE, travessia.getMap().length);
+		System.out.println("initGameTest ok");
 	}
 	@Test
-	public void testIsWinner() {
+	public void IsWinnerTest() {
 	    pos = 10;
 	    MAP_SIZE = 10;
-	    System.out.println("ok");
+	    System.out.println("isWinnerTset ok");
+	    System.out.println("Posicao atual");
+	    System.out.println(pos);
 	}
 	@Test
-	public void testInitGame() {
+	public void InitGameTest() {
+	    pos = 1;
+	    fuel = 6;
+	    map = 10;
+	    System.out.println("InitGameTest ok");
+	    System.out.println("Combustivel atual");
+	    System.out.println(fuel);
+	}
+	@Test
+	public void PrintStatusTest() {
 	    pos = 0;
 	    fuel = 6;
 	    map = 10;
-	    System.out.println("ok");
+	    System.out.println("PrintStatusTest OK");
+	    System.out.println("Posicao atual");
+	    System.out.println(pos);
+
 	}
 	@Test
-	public void testPrintStatus() {
-	    pos = 0;
-	    fuel = 6;
-	    map = 10;
-	    System.out.println("OK");
+	public void PrintStatusTest2() {
+	    pos = 4;
+	    fuel = 8;
+	    map = 9;
+	    System.out.println("PrintStatusTest2 OK");
+	    System.out.println("Combustivel atual");
+	    System.out.println(fuel);
 	}
 	@Test
-	public void testDescarregar() {
-	    fuel = 1;
+	public void carregarTest() {
+	    fuel = 4;
+	    pos = 11;
+	    fuel++;
+	    map--;
+	    System.out.println("carregarTest ok");
+	    System.out.println("Combustivel atual");
+	    System.out.println(fuel);
+	}
+	@Test
+	public void DescarregarTest() {
+	    fuel = 2;
 	    fuel--;
 	    map++;
-	    System.out.println("ok");
+	    System.out.println("DescarregarTest ok");
 	}
+	@Test
+	public void DescarregarTest2() {
+	    fuel = 4;
+	    fuel--;
+	    map--;
+	    System.out.println("DescarregarTest2 ok");
+	}
+	
 }
